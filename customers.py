39c9f73a2336e6fa61ed6ff0cf9576a076c0c2e1
ashkandma <ashkanhasani31@gmail.com>
2024-01-24 07:27:58 +0330

    define signin class

--- customers.py
+++ customers.py
@@ -3,8 +3,25 @@ from users import User
 
 
 class Customer(User):
+    username_list = []
+
     def __init__(self, first_name, last_name, phone, username, password):
         super().__init__(first_name, last_name, phone)
         self.username = username
         self.password = password
         self.user_id = uuid.uuid4()
+        Customer.username_list.append(self.username)
+
+    @classmethod
+    def signin(cls):
+        fname = input(">>> Enter your first name: ")
+        lname = input(">>> Enter your last name: ")
+        phone = input(">>> Enter your phone number")
+        while True:
+            username = input(">>> Enter username: ")
+            if username not in Customer.username_list:
+                break
+            else:
+                print("your username is exist, Try another one")
+        password = input(">>> Enter password")
+        return cls(fname, lname, phone, username, password)
