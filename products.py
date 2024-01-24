cfc2de5a315c1bf805d001c9670afe50c186cc1d
ashkandma <ashkanhasani31@gmail.com>
2024-01-24 07:46:07 +0330

    define a function for add new product

--- products.py
+++ products.py
@@ -6,4 +6,13 @@ class Product:
         self.description = description
         self.category = category
         self.image = image
-       
+
+    @classmethod
+    def add(cls):
+        name = input(">>> Enter the product name: ")
+        price = int(input(">>> Enter the product price: "))
+        stock = int(input(">>> Enter the product first stock: "))
+        description = input(">>> Enter the product description: ")
+        category = input(">>> Enter the product category: ")
+        image = input(">>> Enter the url of product image: ")
+        return cls(name, price, stock, description, category, image)
