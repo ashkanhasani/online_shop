8a95bb04dd1d4c62942f31e22c15b1a3e8936785
ashkandma <ashkanhasani31@gmail.com>
2024-01-24 07:41:25 +0330

    define Product class

---
+++ products.py
@@ -0,0 +1,9 @@
+class Product:
+    def __init__(self, name, price, stock, description, category, image):
+        self.name = name
+        self.price = price
+        self.stock = stock
+        self.description = description
+        self.category = category
+        self.image = image
+       
